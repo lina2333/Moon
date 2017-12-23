5666dda36f82a21bbd0e3b815c06026bd86fdb0b
zengyazhi <yazhi1992@163.com>
2017-12-23 15:57:38 +0800

    add test2 in main2

--- app/src/main/java/com/yazhi1992/moon/Main2Activity.kt
+++ app/src/main/java/com/yazhi1992/moon/Main2Activity.kt
@@ -6,6 +6,7 @@ import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
     //test1
+    //add by test2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
